de64b3de7eb913e28eec7ca31b75021d7b7ca750
kpol <kirpolis@gmail.com>
2018-07-01 17:13:29 +1200

    Benchmarks

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ExtraLinq;
+
+namespace Benchmark
+{
+    public class Md5VsSha256
+    {
+        private readonly long[] _data;
+
+        private readonly List<long> _dataList;
+
+        public Md5VsSha256()
+        {
+            const int count = 1000000;
+            var random = new Random().Next(5000);
+            _data = Enumerable.Range(random, random + count).Select(i => (long) i).ToArray();
+            _dataList = _data.ToList();
+        }
+
+        [Benchmark]
+        public long SumArrayLinq() => Enumerable.Sum(_data);
+
+        [Benchmark]
+        public long SumArrayExtraLinq() => ExtraEnumerable.Sum(_data);
+
+        [Benchmark]
+        public long SumListLinq() => Enumerable.Sum(_dataList);
+
+        [Benchmark]
+        public long SumListExtraLinq() => ExtraEnumerable.Sum(_dataList);
+
+        [Benchmark]
+        public long MaxLinq() => Enumerable.Max(_data);
+
+        [Benchmark]
+        public long MaxByExtraLinq() => ExtraEnumerable.MaxBy(_data, i => i);
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+        }
+    }
+}
--- src/ExtraLinq/MaxMinBy.cs
+++ src/ExtraLinq/MaxMinBy.cs
@@ -32,6 +32,33 @@ namespace ExtraLinq
             return GetExtremaArray(source, selector, (x, y) => comparer.Compare(x, y));
         }
 
+        public static TSource MaxBy<TSource, TKey>(this List<TSource> source,
+            Func<TSource, TKey> selector, IComparer<TKey> comparer = null)
+        {
+            if (source == null) throw Error.ArgumentNull(nameof(source));
+            if (selector == null) throw Error.ArgumentNull(nameof(selector));
+
+            comparer = comparer ?? Comparer<TKey>.Default;
+
+            if (source.Count == 0)
+            {
+                throw Error.NoElements();
+            }
+
+            return GetExtremaList(source, selector, (x, y) => comparer.Compare(x, y));
+        }
+
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector, IComparer<TKey> comparer = null)
+        {
+            if (source == null) throw Error.ArgumentNull(nameof(source));
+            if (selector == null) throw Error.ArgumentNull(nameof(selector));
+
+            comparer = comparer ?? Comparer<TKey>.Default;
+
+            return GetExtrema(source, selector, (x, y) => comparer.Compare(y, x));
+        }
+
         public static TSource MinBy<TSource, TKey>(this TSource[] source,
             Func<TSource, TKey> selector, IComparer<TKey> comparer = null)
         {
@@ -48,7 +75,7 @@ namespace ExtraLinq
             return GetExtremaArray(source, selector, (x, y) => comparer.Compare(y, x));
         }
 
-        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
+        public static TSource MinBy<TSource, TKey>(this List<TSource> source,
             Func<TSource, TKey> selector, IComparer<TKey> comparer = null)
         {
             if (source == null) throw Error.ArgumentNull(nameof(source));
@@ -56,7 +83,12 @@ namespace ExtraLinq
 
             comparer = comparer ?? Comparer<TKey>.Default;
 
-            return GetExtrema(source, selector, (x, y) => comparer.Compare(y, x));
+            if (source.Count == 0)
+            {
+                throw Error.NoElements();
+            }
+
+            return GetExtremaList(source, selector, (x, y) => comparer.Compare(y, x));
         }
 
         private static TSource GetExtrema<TSource, TKey>(IEnumerable<TSource> source,
@@ -95,6 +127,27 @@ namespace ExtraLinq
             var extremaKey = selector(extrema);
 
             for (int i = 1; i < source.Length; i++)
+            {
+                var candidate = source[i];
+                var candidateProjected = selector(candidate);
+
+                if (funcComparer(extremaKey, candidateProjected) < 0)
+                {
+                    extrema = candidate;
+                    extremaKey = candidateProjected;
+                }
+            }
+
+            return extrema;
+        }
+
+        private static TSource GetExtremaList<TSource, TKey>(List<TSource> source,
+            Func<TSource, TKey> selector, Func<TKey, TKey, int> funcComparer)
+        {
+            var extrema = source[0];
+            var extremaKey = selector(extrema);
+
+            for (int i = 1; i < source.Count; i++)
             {
                 var candidate = source[i];
                 var candidateProjected = selector(candidate);
