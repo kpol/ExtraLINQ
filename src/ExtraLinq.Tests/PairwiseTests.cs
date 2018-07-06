eec2261c6ba1cb99b2db2faa3bb696eef1fc7e0b
kpol <kirpolis@gmail.com>
2018-07-07 11:10:41 +1200

    Pairwise

---
+++ src/ExtraLinq.Tests/PairwiseTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtraLinq.Tests
+{
+    [TestClass]
+    public class PairwiseTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PairwiseSourceIsNull()
+        {
+            var _ = ((IEnumerable<int>) null).Pairwise((x, y) => new {x, y});
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PairwiseSelectorIsNull()
+        {
+            var _ = Enumerable.Range(1, 5).Pairwise((Func<int, int, int>) null);
+        }
+
+        [TestMethod]
+        public void PairwiseSourceEmpty()
+        {
+            var result = Enumerable.Range(1, 0).Pairwise((x, y) => new Tuple<int, int>(x, y));
+
+            CollectionAssert.AreEquivalent(Enumerable.Empty<Tuple<int, int>>().ToArray(), result.ToArray());
+        }
+
+        [TestMethod]
+        public void PairwiseSourceOneItem()
+        {
+            var result = Enumerable.Range(1, 1).Pairwise((x, y) => new Tuple<int, int>(x, y));
+
+            CollectionAssert.AreEquivalent(Enumerable.Empty<Tuple<int, int>>().ToArray(), result.ToArray());
+        }
+
+        [TestMethod]
+        public void Pairwise()
+        {
+            var result = Enumerable.Range(1, 5).Pairwise((x, y) => new Tuple<int, int>(x, y));
+
+            CollectionAssert.AreEquivalent(new[]
+            {
+                new Tuple<int, int>(1, 2),
+                new Tuple<int, int>(2, 3),
+                new Tuple<int, int>(3, 4),
+                new Tuple<int, int>(4, 5),
+            }, result.ToArray());
+        }
+    }
+}
---
+++ src/ExtraLinq/Pairwise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class ExtraEnumerable
+    {
+        public static IEnumerable<TResult> Pairwise<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TSource, TResult> selector)
+        {
+            if (source == null) throw Error.ArgumentNull(nameof(source));
+            if (selector == null) throw Error.ArgumentNull(nameof(selector));
+
+            return _();
+
+            IEnumerable<TResult> _()
+            {
+                using (var enumerator = source.GetEnumerator())
+                {
+                    if (!enumerator.MoveNext())
+                    {
+                        yield break;
+                    }
+
+                    var current = enumerator.Current;
+
+                    while (enumerator.MoveNext())
+                    {
+                        yield return selector(current, enumerator.Current);
+                        current = enumerator.Current;
+                    }
+                }
+            }
+        }
+    }
+}
