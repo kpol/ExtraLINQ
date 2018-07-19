56490b7f950c853372141be62f1038a4e70e9784
Kirill Polishchuk <kirpolis@gmail.com>
2018-07-19 15:09:48 +1200

    ToHashSet

---
+++ src/ExtraLinq/ToHashSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class ExtraEnumerable
+    {
+        public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer = null)
+        {
+            if (source == null) throw Error.ArgumentNull(nameof(source));
+
+            return new HashSet<TSource>(source, comparer);
+        }
+    }
+}
