67dfef226826858e57f318cb3cb31eab1fb371f7
Kirill Polishchuk <kirpolis@gmail.com>
2018-07-19 14:08:58 +1200

    ForEach

---
+++ src/ExtraLinq.Tests/ForEachTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtraLinq.Tests
+{
+    [TestClass]
+    public class ForEachTests
+    {
+        [TestMethod]
+        public void ForEachEnumerable()
+        {
+            var source = Enumerable.Range(1, 2);
+
+            var sb = new StringBuilder();
+
+            source.ForEach((x, i) => { sb.Append($"{{{x},{i}}}");});
+
+            Assert.AreEqual("{1,0}{2,1}", sb.ToString());
+        }
+    }
+}
---
+++ src/ExtraLinq/ForEach.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtraLinq
+{
+    public static partial class ExtraEnumerable
+    {
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            if (source == null) throw Error.ArgumentNull(nameof(source));
+            if (action == null) throw Error.ArgumentNull(nameof(action));
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource, int> action)
+        {
+            if (source == null) throw Error.ArgumentNull(nameof(source));
+            if (action == null) throw Error.ArgumentNull(nameof(action));
+
+            var index = 0;
+
+            foreach (var item in source)
+            {
+                action(item, index++);
+            }
+        }
+    }
+}
