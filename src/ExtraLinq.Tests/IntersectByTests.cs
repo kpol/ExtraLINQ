51f4424fa6487a74a565a3f41e7103cc4752db77
Kirill Polishchuk <kirpolis@gmail.com>
2018-07-23 12:05:34 +1200

    IntersectBy

---
+++ src/ExtraLinq.Tests/IntersectByTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtraLinq.Tests
+{
+    [TestClass]
+    public class IntersectByTests
+    {
+        [TestMethod]
+        public void IntersectByEnumearble()
+        {
+            var first = Enumerable.Range(1, 5).Select(x => new Data {Id = x, Text = (x * 10).ToString()});
+            var second = Enumerable.Range(3, 5).Select(x => new Data { Id = x, Text = (x * 10).ToString() });
+
+            var result = first.IntersectBy(second, x => x.Id).ToArray();
+
+            Assert.AreEqual(3, result.Length);
+            CollectionAssert.AreEquivalent(new[] {3, 4, 5}, result.Select(x => x.Id).ToArray());
+            CollectionAssert.AreEquivalent(new[] { "30", "40", "50" }, result.Select(x => x.Text).ToArray());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectBySourceIsNull()
+        {
+            var _ = ((IEnumerable<int>)null).IntersectBy(Enumerable.Range(1, 10), x => x);
+        }
+
+        private class Data
+        {
+            public int Id { get; set; }
+
+            public string Text { get; set; }
+        }
+    }
+}
---
+++ src/ExtraLinq/IntersectBy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtraLinq
+{
+    public static partial class ExtraEnumerable
+    {
+        public static IEnumerable<TSource> IntersectBy<TSource, TKey>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second, 
+            Func<TSource, TKey> keySelector,
+            IEqualityComparer<TKey> keyComparer = null)
+        {
+            if (first == null) throw Error.ArgumentNull(nameof(first));
+            if (second == null) throw Error.ArgumentNull(nameof(second));
+            if (keySelector == null) throw Error.ArgumentNull(nameof(keySelector));
+
+            return _();
+
+            IEnumerable<TSource> _()
+            {
+                var keys = new HashSet<TKey>(second.Select(keySelector), keyComparer);
+
+                foreach (var item in first)
+                {
+                    if (keys.Remove(keySelector(item)))
+                    {
+                        yield return item;
+                    }
+                }
+            }
+        }
+    }
+}
